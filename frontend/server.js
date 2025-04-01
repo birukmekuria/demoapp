3d13fe06d97acc28fbed34279002efb8e6392ddf
Biruk Mekuria <brkassefa@gmail.com>
2025-04-01 11:28:38 +0300

    server.js modofied

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,22 @@
-//Import express and path modules
-const express = require("express");
-const path = require("path");
-//Create an instance of express
-const app = express();
-//Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "dist")));
-//Redirect every request to the index.html file
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
-//Listen to the default port 80
-app.listen(80);
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const app = express();
+
+// Fix __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Redirect every request to index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// Listen on port 80
+app.listen(80, () => {
+  console.log("Server is running on http://localhost:80");
+});
